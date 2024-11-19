24f3186290671c6108b145e39256756c1b35d956
David <david.tran@vinova.com.sg>
2024-11-19 16:26:23 +0700

    week1: practice arithmetic exercises

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,7 +1,20 @@
-export const add = () => {};
+export const add = (a, b) => {
+    if(typeof a !== 'number' || typeof b !== 'number') return 'Add values must be number'
+    return a + b
+}
 
-export const subtract = () => {};
+export const substract = (a, b) =>{
+    if(typeof a !== 'number' || typeof b !== 'number') throw 'Substract values must be number'
+    return a - b
+}
 
-export const multiply = () => {};
+export const multiply = (a, b) =>{
+    if(typeof a !== 'number' || typeof b !== 'number') throw 'Multiply values must be number'
+    return a * b
+}
 
-export const divide = () => {};
+export const divide = (a, b) =>{
+    if(typeof a !== 'number' || typeof b !== 'number') return 'Divide values must be number'
+    if(b === 0) throw 'b value must not be 0'
+    return a / b
+}
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,9 +1,35 @@
-import { describe, it, expect } from 'vitest';
+import { add, substract, divide, multiply } from './arithmetic'
 
-describe.todo('add', () => {});
+describe('add', () => {
+    test('Add 34 + 43 = 77', () => {
+        expect(add(34, 43)).toBe(77);
+        expect(add(0.1, 0.2)).toBeCloseTo(0.3);
+    })
+});
 
-describe.todo('subtract', () => {});
+describe('subtract', () => {
+    it('Subtract 49 - 24 = 25', () => {
+        expect(substract(49, 24)).toBe(25);
+        expect(() => substract(21, undefined)).toThrow('Substract values must be number');
+    })
+    
+});
 
-describe.todo('multiply', () => {});
+describe('multiply', () => {
+    describe('Multiply 24 * 8 = 192', () => {
+        expect(multiply(24, 8)).toBe(192);
+    
+        
+        test('Nested multiply', () => {
+            expect(multiply(21, 4)).toBe(84)
+        })
+    })
+    
+});
 
-describe.todo('divide', () => {});
+describe('divide', () => {
+    test('Divide 121 / 11 = 11', () => {
+        expect(divide(121, 11)).toBe(11);
+        expect(() => divide(5, 0)).toThrow('b value must not be 0')
+    })
+});
